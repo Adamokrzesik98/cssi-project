dc91b37fe07095650ffb4bdca3ef4b0f8b9745cb
Adamokrzesik98 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-01 11:32:16 -0500

    Displays who has activated DND

--- roommates/helpers.py
+++ roommates/helpers.py
@@ -21,6 +21,7 @@ def getDashData(self, person):
 			checked_in = []
 			checked_out = []
 			people_in_home = []
+			has_dnd_on = []
 			for id in home[0].occupants:
 				people_in_home.append(Person.query().filter(id == Person.user_id).fetch()[0])
 
@@ -31,14 +32,15 @@ def getDashData(self, person):
 					checked_out.append(person)
 				if p.do_not_disturb:
 					dnd_state = True
+					has_dnd_on.append(p)
 			for sticky_note in Sticky.query().filter(Sticky.home_key == person.home_key).fetch():
 				home_stickies.append(sticky_note)
-			
+
 
 				
 
 			# for person in people_in_home:
 			# 	logging.info(person.name)
 
-			return_data = {'checked_in' : checked_in, 'checked_out' : checked_out, 'dnd' : dnd_state, 'home_stickies' : home_stickies}
+			return_data = {'checked_in' : checked_in, 'checked_out' : checked_out, 'dnd' : dnd_state, 'has_dnd_on' : has_dnd_on ,'home_stickies' : home_stickies}
 			return return_data
