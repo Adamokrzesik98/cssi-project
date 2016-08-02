897ed45c2c2d217ec29335bc7f2d8bbc474e17ac
Doug Smith <cssi2016@CSSIs-MacBook-Air.local>
2016-08-02 14:50:12 -0500

    Nothing

--- roommates/my_calendar.py
+++ roommates/my_calendar.py
@@ -1,7 +1,9 @@
-from oauth2client import client
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import OAuth2WebServerFlow
+
 
 def get_credentials():
-	flow = client.flow_from_clientsecrets(
-	    'client_secret_tester.json',
-	    scope='https://www.googleapis.com/auth/calendar.readonly',
-	    redirect_uri='http://www.example.com/oauth2callback')
+	flow = OAuth2WebServerFlow(client_id='270567588357-nnleha1dmgvgr7jatb1du51ruvqn4mou.apps.googleusercontent.com',
+                           client_secret='cAn4N7YIjckbjRaHWNqp1OEZ',
+                           scope='https://www.googleapis.com/auth/calendar',
+                           redirect_uri='http://localhost:9080/receive_credentials')
