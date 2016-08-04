0df096e0c017246c1fc64ae164c6be67f4e7f4fb
Doug Smith <cssi2016@CSSIs-MacBook-Air.local>
2016-08-04 14:42:47 -0500

    bugs

--- roommates/helpers.py
+++ roommates/helpers.py
@@ -17,24 +17,24 @@ from httplib2 import Http
 from googleapiclient.discovery import build
 
 def createNewCal(self):
-    service = build('calendar', 'v3')
+	service = build('calendar', 'v3')
 
-    scopes = ['https://www.googleapis.com/auth/calendar']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('service_account.json', scopes=scopes)
-    http_auth = credentials.authorize(Http())
-    # delegated_credentials = credentials.create_delegated(person.email_address)
-    # http_auth = delegated_credentials.authorize(httplib2.Http())
-    calendar = {
-        'summary': "Home Name's Calendar",
-        'timeZone': 'America/Los_Angeles'
-    }
-    new_calendar = service.calendars().insert(body=calendar).execute(http=http_auth)
-    logging.info(new_calendar['id'])
-    return new_calendar['id']
+	scopes = ['https://www.googleapis.com/auth/calendar']
+	credentials = ServiceAccountCredentials.from_json_keyfile_name('service_account.json', scopes=scopes)
+	http_auth = credentials.authorize(Http())
+	# delegated_credentials = credentials.create_delegated(person.email_address)
+	# http_auth = delegated_credentials.authorize(httplib2.Http())
+	calendar = {
+		'summary': "Home Name's Calendar",
+		'timeZone': 'America/Los_Angeles'
+	}
+	new_calendar = service.calendars().insert(body=calendar).execute(http=http_auth)
+	logging.info(new_calendar['id'])
+	return new_calendar['id']
 
 def hashPass(password):
-    m = hashlib.sha256(password).hexdigest()
-    return m
+	m = hashlib.sha256(password).hexdigest()
+	return m
 
 # redirect current page to address
 def redirect(self, address, wait_time):
@@ -45,50 +45,50 @@ def redirect(self, address, wait_time):
 #Takes input as a person
 #Returns a dictionary of DND status, current stickies, and people checked in
 def getDashData(self, person):
-        if person:
-            home = Home.query().filter(Home.key == person.home_key).fetch()
-            dnd_state = False
-            home_stickies = []
-            checked_in = []
-            checked_out = []
-            people_in_home = []
-            has_dnd_on = []
-            for id in home[0].occupants:
-                people_in_home.append(Person.query().filter(id == Person.user_id).fetch()[0])
-            for p in people_in_home:
-                if p.location:
-                    checked_in.append(p)
-                else:
-                    checked_out.append(p)
-                if p.do_not_disturb:
-                    has_dnd_on.append(p)
-            # Check for and delete expired sticky notes
-            for sticky_note in Sticky.query().filter(Sticky.home_key == person.home_key).fetch():
-                if sticky_note.expiration < time.time():
-                    sticky_note.key.delete()
-                else:
-                    home_stickies.append(sticky_note)
-            # fetch chores
-            chores = Chore.query().filter(Chore.home_key==home[0].key).fetch()
-            # update chores
-            for chore in chores:
-                if chore.end_time < time.time():
-                    chore.end_time = chore.end_time + chore.duration
-                    chore.index = (chore.index + 1)%len(chore.workers)
-                    chore.completed = False
-                    chore.put()
-            # fetch bills
-            bills = Bills.query().filter(Bills.home_key==home[0].key).fetch()
+		if person:
+			home = Home.query().filter(Home.key == person.home_key).fetch()
+			dnd_state = False
+			home_stickies = []
+			checked_in = []
+			checked_out = []
+			people_in_home = []
+			has_dnd_on = []
+			for id in home[0].occupants:
+				people_in_home.append(Person.query().filter(id == Person.user_id).fetch()[0])
+			for p in people_in_home:
+				if p.location:
+					checked_in.append(p)
+				else:
+					checked_out.append(p)
+				if p.do_not_disturb:
+					has_dnd_on.append(p)
+			# Check for and delete expired sticky notes
+			for sticky_note in Sticky.query().filter(Sticky.home_key == person.home_key).fetch():
+				if sticky_note.expiration < time.time():
+					sticky_note.key.delete()
+				else:
+					home_stickies.append(sticky_note)
+			# fetch chores
+			chores = Chore.query().filter(Chore.home_key==home[0].key).fetch()
+			# update chores
+			for chore in chores:
+				if chore.end_time < time.time():
+					chore.end_time = chore.end_time + chore.duration
+					chore.index = (chore.index + 1)%len(chore.workers)
+					chore.completed = False
+					chore.put()
+			# fetch bills
+			bills = Bills.query().filter(Bills.home_key==home[0].key).fetch()
 
 
-            # fetch room name
-            room_name = home[0].name
+			# fetch room name
+			room_name = home[0].name
 
-            # for person in people_in_home:
-            #   logging.info(person.name)
+			# for person in people_in_home:
+			#   logging.info(person.name)
 
-            return_data = {'room_name': room_name, 'bills': bills, 'chores': chores, 'checked_in' : checked_in, 'checked_out' : checked_out, 'has_dnd_on' : has_dnd_on ,'home_stickies' : home_stickies, 'person': person}
-            return return_data
+			return_data = {'room_name': room_name, 'bills': bills, 'chores': chores, 'checked_in' : checked_in, 'checked_out' : checked_out, 'has_dnd_on' : has_dnd_on ,'home_stickies' : home_stickies, 'person': person}
+			return return_data
 
 
 #Currently Unused
@@ -104,15 +104,15 @@ def dndEnabled(self, enabler):
 
 
 def send_dnd_mail(sender_address, name, receipient_address):
-    # [START send_message]
-    message = mail.EmailMessage(
-        sender=sender_address,
-        subject= "ALERT: " + name + " has turned on Do Not Disturb")
+	# [START send_message]
+	message = mail.EmailMessage(
+		sender=sender_address,
+		subject= "ALERT: " + name + " has turned on Do Not Disturb")
 
-    message.to = receipient_address
-    message.body = "ALERT: " + name + " has turned on Do Not Disturb.  From, your friends at Roommates"
-    message.send()
-    # [END send_message]
+	message.to = receipient_address
+	message.body = "ALERT: " + name + " has turned on Do Not Disturb.  From, your friends at Roommates"
+	message.send()
+	# [END send_message]
 
 def removeFromRoom(self, user):
 	person = login.is_roommate_account_initialized(user)
@@ -143,7 +143,8 @@ def removeFromRoom(self, user):
 			home.key.delete()
 		else: #Creo que esto funcione
 			for c in chores:
-				c.workers.remove(person.user_id)
+				if person.user_id in c.workers:
+					c.workers.remove(person.user_id)
 			for b in bills:
 				if b.payer_id == person.user_id:
 					b.key.delete()
@@ -158,21 +159,16 @@ def removeFromRoom(self, user):
 
 
 
-
-
-
-
-
 def sendSMS(to, message):
-    # replace with your credentials from: https://www.twilio.com/user/account
-    account_sid = "SK205e8dc6c1bcdc8d8f1f1cffd7bc79e9"
-    auth_token = "BPEfbiNoBLcGJqmix1G2xGrkAFT175ei"
-    client = TwilioRestClient(account_sid, auth_token)
-    # replace "to" and "from_" with real numbers
-    rv = client.messages.create(to="+1" + to,
-                                from_="+16304493710",
-                                body=message)
-    self.response.write(str(rv))
+	# replace with your credentials from: https://www.twilio.com/user/account
+	account_sid = "SK205e8dc6c1bcdc8d8f1f1cffd7bc79e9"
+	auth_token = "BPEfbiNoBLcGJqmix1G2xGrkAFT175ei"
+	client = TwilioRestClient(account_sid, auth_token)
+	# replace "to" and "from_" with real numbers
+	rv = client.messages.create(to="+1" + to,
+								from_="+16304493710",
+								body=message)
+	self.response.write(str(rv))
 
 
 
